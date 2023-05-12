7f181551048f900ec2e73cdd66c96a722dd03c15
Alexis Faure <alexis.faure@epitech.eu>
2023-05-12 15:11:16 +0200

    [ADD] - Timeout on tests

--- tests/UserService.test.ts
+++ tests/UserService.test.ts
@@ -13,53 +13,53 @@ describe("UserService", () => {
         if (!user) {
             throw new Error("Could not find user");
         }
-    });
+    }, 30000);
     it("should return a valid user name", async () => {
         const client = new EpitechClient(process.env.EPITECH_COOKIE as string);
         const name = await client.user.getStudentName();
         expect(["Anonymous user", "Alexis FAURE"]).toContain(name);
         return;
-    });
+    }, 30000);
     it("should return a valid user promo", async () => {
         const client = new EpitechClient(process.env.EPITECH_COOKIE as string);
         const promo = await client.user.getStudentPromo();
         expect(promo).toBe(user.promo);
         return;
-    });
+    }, 30000);
     it("should return a valid user year", async () => {
         const client = new EpitechClient(process.env.EPITECH_COOKIE as string);
         const year = await client.user.getStudentYear();
         expect(year).toBe(user.studentyear);
         return;
-    });
+    }, 30000);
     it("should return a valid user picture", async () => {
         const client = new EpitechClient(process.env.EPITECH_COOKIE as string);
         const picture = await client.user.getPicture();
         expect(picture.startsWith("https://intra.epitech.eu/file/userprofil/profilview/") && picture.endsWith("@epitech.eu.jpg")).toBe(true);
         return;
-    });
+    }, 30000);
     it("should return a valid user location", async () => {
         const client = new EpitechClient(process.env.EPITECH_COOKIE as string);
         const location = await client.user.getStudentLocation();
         expect(location).toBe(user.location);
         return;
-    });
+    }, 30000);
     it("should return a valid user gpa", async () => {
         const client = new EpitechClient(process.env.EPITECH_COOKIE as string);
         const gpa = await client.user.getStudentGPA();
         expect(gpa).toBe(user.gpa[0].gpa);
         return;
-    });
+    }, 30000);
     it("should return a valid user credits", async () => {
         const client = new EpitechClient(process.env.EPITECH_COOKIE as string);
         const credits = await client.user.getStudentCredits();
         expect(credits).toBe(user.credits);
         return;
-    });
+    }, 30000);
     it("should return valid user informations", async () => {
         const client = new EpitechClient(process.env.EPITECH_COOKIE as string);
         const partialUser = await client.user.getAllStudentInfo();
         expect(partialUser.login).toBe(user.login);
         return;
-    });
+    }, 30000);
 });
