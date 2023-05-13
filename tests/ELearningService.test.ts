0c0c01782883057c1dc671c1da2e94c0b4af52f9
Alexis Faure <alexis.faure@epitech.eu>
2023-05-13 12:47:55 +0200

    [FIX] - Fix elearning test that fail in CI

--- tests/ELearningService.test.ts
+++ tests/ELearningService.test.ts
@@ -3,12 +3,16 @@ import ElearningResponse, { Step } from "../src/types/ELearning";
 
 describe("ELearningService", () => {
   test("should failed when using invalid cookie", (done) => {
-    const client = new EpitechClient(process.env.EPITECH_COOKIE as string);
-    client.elearning.setCookie("user=invalid");
-    client.elearning.getELearning().catch((err: Error) => {
-      expect(err.message).toBe("Request failed with status code 403");
-      done();
-    });
+    try {
+      const client = new EpitechClient();
+      client.elearning.setCookie("invalid");
+      client.elearning.getELearning().catch((err: Error) => {
+        expect(err.message).toBe("Request failed with status code 403");
+        done();
+      });
+    } catch (err) {
+      done(err);
+    }
   }, 30000);
   test("should return valid elearning", (done) => {
     const client = new EpitechClient(process.env.EPITECH_COOKIE as string);
